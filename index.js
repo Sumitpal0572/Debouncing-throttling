ef9c870abcc80b9fb573cb2f6f72f357d4abeeb3
Sumitpal0572 <asmyselfsumit@gmail.com>
2024-07-28 00:00:01 +0530

    second commit

--- index.js
+++ index.js
@@ -3,5 +3,17 @@ let input1 = document.getElementById("input");
 let btn = document.getElementById("btn");
 
 btn.addEventListener("click", function () {
-    fetchdata()
+    throttle(fetchdata, 2000)
 })
+
+let flag = flase
+function throttle(func, delay) {
+    if (flag) {
+        return
+    }
+    func();
+    flag = true;
+    setTimeout(() => {
+        flag = false
+    }, delay)
+}
