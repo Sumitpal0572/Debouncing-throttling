9d1399ced2f98f8a213833acc9167e752149e3f3
Sumitpal0572 <asmyselfsumit@gmail.com>
2024-07-29 00:16:12 +0530

    second commit

--- Assign2nd/app.js
+++ Assign2nd/app.js
@@ -3,8 +3,20 @@ let url = ` http://www.omdbapi.com/?i=tt3896198&apikey=92212ae5`
 
 let search = document.getElementById("search")
 search.addEventListener("input", function () {
-    fetchdata()
+    debounce(fetchdata, 1000)
 });
+
+let id;
+function debounce(func, delay) {
+    if (id) {
+        clearTimeout(id)
+    }
+
+    id = setTimeout(() => {
+        func()
+    }, delay)
+}
+
 
 function fetchdata() {
     console.log("hello");
