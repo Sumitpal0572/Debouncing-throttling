4861fe640df1829e199bd547b997b9947868080c
Sumitpal0572 <asmyselfsumit@gmail.com>
2024-07-30 20:00:23 +0530

    debouncing done

--- Assign2nd/app.js
+++ Assign2nd/app.js
@@ -42,3 +42,37 @@ function displaydata(data) {
     })
 }
 
+
+async function fetchMovieDetails(imdbID) {
+    try {
+        let res = await fetch(
+            `http://www.omdbapi.com/?i=${imdbID}&apikey=f9f6a9fb`
+        );
+        let data = await res.json();
+        console.log(data);
+        displayMovieDetails(data);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function displayMovieDetails(data) {
+    let container = document.getElementById("container");
+    container.innerHTML = "";
+
+    let title = document.createElement("h2");
+    title.textContent = data.Title;
+
+    let poster = document.createElement("img");
+    poster.src = data.Poster;
+
+    let year = document.createElement("p");
+    year.textContent = `Year: ${data.Year}`;
+
+    let genre = document.createElement("p");
+    genre.textContent = `Genre: ${data.Genre}`;
+
+    let plot = document.createElement("p");
+    plot.textContent = `Plot: ${data.Plot}`;
+    container.append(poster, title, year, genre, plot);
+}
